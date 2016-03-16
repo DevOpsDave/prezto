1bc60abef381a2a79fb8a3e38072608a9fb57f53
Dave Barcelo <dbarcelo@salesforce.com>
2016-03-16 15:16:48 -0400

    Changes I don't remember and my ssh agent change

--- runcoms/zprofile.d/boot2docsker.zsh
+++ runcoms/zprofile.d/boot2docsker.zsh
@@ -1,3 +1,4 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/dabarcel/.boot2docker/certs/boot2docker-vm
+#export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_HOST=tcp://127.0.0.1:2376
+export DOCKER_CERT_PATH=/Users/dbarcelo/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
